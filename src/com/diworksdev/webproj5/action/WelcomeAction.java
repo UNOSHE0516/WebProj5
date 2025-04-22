55a74c55eb85ffdca1e20f386571235c04d9b434
UNOSHE0516 <hiro0516jms@gmail.com>
2025-04-22 21:10:00 +0900

    welcome.jsp作成

--- src/com/diworksdev/webproj5/action/WelcomeAction.java
+++ src/com/diworksdev/webproj5/action/WelcomeAction.java
@@ -1,5 +1,9 @@
 package com.diworksdev.webproj5.action;
 
-public class WelcomeAction {
+import com.opensymphony.xwork2.ActionSupport;
 
+public class WelcomeAction extends ActionSupport{
+	public String execute() {
+		return SUCCESS;
+	}
 }
